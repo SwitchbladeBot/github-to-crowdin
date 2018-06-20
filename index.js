e57e3600df23c794a80455ad6cab7bc5d742b9d3
Pedro Fracassi <pedroluizfracassi@gmail.com>
2018-06-19 22:07:52 -0300

    Add basic functionality

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const GithubWebHook = require('express-github-webhook');
+const webhookHandler = GithubWebHook({ path: '/', secret: process.env.WEBHOOK_SECRET });
+const app = express()
+
+app.use(bodyParser.json())
+app.use(webhookHandler)
+
+webhookHandler.on('*', (event, repo, data) => {
+  console.log(event)
+  console.log(repo)
+  console.log(data)
+})
+
+app.listen(80);
